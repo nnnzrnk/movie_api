d7b41d41cba5f9d656ffb881c830ea3526d1fcc7
nnnzrnk <127133520+nnnzrnk@users.noreply.github.com>
2023-09-01 16:14:15 +0200

    completed 2.4

--- index.js
+++ index.js
@@ -1,27 +1,50 @@
-const url = require('url');
-const fs = require('fs');
-const http = require('http');
+// const url = require('url');
+// const fs = require('fs');
+// const http = require('http');
 
-let server = http.createServer((request, response) => {
-    response.writeHead(200, {'Content-type': 'text/html; charset=utf-8'})
-    if(request.url == '/')
-    fs.createReadStream('./index.html').pipe(response)
-    else if(request.url == '/documentation')
-    fs.createReadStream('./documentation.html').pipe(response)
+// let server = http.createServer((request, response) => {
+//     response.writeHead(200, {'Content-type': 'text/html; charset=utf-8'})
+//     if(request.url == '/documentation')
+//     fs.createReadStream('./documentation.html').pipe(response)
 
-    fs.appendFile('log.txt', 'URL: ' + request.url + '\nTimestamp: ' + new Date() + '\n\n', (err) => {
-        if (err) {
-          console.log(err);
-        } else {
-          console.log('Added to log.');
-        }
-      });
+//     fs.appendFile('log.txt', 'URL: ' + request.url + '\nTimestamp: ' + new Date() + '\n\n', (err) => {
+//         if (err) {
+//           console.log(err);
+//         } else {
+//           console.log('Added to log.');
+//         }
+//       });
 
-})
+// })
 
 const PORT = 8080
 const HOST = 'localhost'
 
-server.listen(PORT, HOST, () => {
-    console.log(`http://${HOST}:${PORT}`)
+// server.listen(PORT, HOST, () => {
+//     console.log(`http://${HOST}:${PORT}`)
+// })
+
+let express = require('express')
+let app = express()
+app.use(express.static('public'))
+let morgan = require('morgan')
+
+app.use(morgan('common'))
+
+app.get('/movies', (req, res) => {
+  res.json(topMovies)
 })
+
+app.get('/', (req, res) => {
+  res.send('This is my top list of movies')
+})
+
+app.use((err, req, res, next) => {
+  console.error(err.stack)
+  res.status(500).send('Warning!')
+})
+
+app.listen(8080, () => {
+  console.log(`http://${HOST}:${PORT}`)
+})
+
