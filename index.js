34b8f32794f18882a8ebb6a774bf64b6d92f3bf4
nnnzrnk <127133520+nnnzrnk@users.noreply.github.com>
2023-09-15 18:33:53 +0200

    added input validation

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const HOST = "localhost";
 
 const express = require("express");
@@ -12,6 +12,8 @@ const { title } = require("process");
 const mongoose = require('mongoose');
 const Models = require('./models');
 const { error } = require("console");
+const { check, validationResult } = require('express-validator'); 
+
 
 const movies = Models.movie;
 const users = Models.user;
@@ -22,6 +24,8 @@ app.use(bodyParser.json());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
 
+const cors = require('cors')
+app.use(cors())
 let auth = require('./auth')(app)
 const passport = require('passport')
 require('./passport')
@@ -29,13 +33,13 @@ require('./passport')
 const accessLogStream = fs.createWriteStream(path.join(__dirname, "log.txt"), {
   flags: "a",
 });
-// app.use(morgan('common'))
-app.use(morgan("combined", { stream: accessLogStream }));
+app.use(morgan('common'))
+// app.use(morgan("combined", { stream: accessLogStream }));
 
 app.get("/movies", passport.authenticate('jwt', {session: false}),
 async (req, res) => {
  await movies.find().then((movies) => {
-    res.status(201).json(movies);
+    res.status(200).json(movies);
   })
   .catch((err) => {
     console.error(err);
@@ -44,9 +48,9 @@ async (req, res) => {
 });
 
 app.get("/users",passport.authenticate('jwt', {session: false}),
-async (req, res) => {
- await users.find().then((users) => {
-   res.status(201).json(users)
+(req, res) => {
+ users.find().then((users) => {
+   res.status(200).json(users)
   }).catch((err) => {
    console.error(err);
    res.status(500).send('Error '+ err)
@@ -56,8 +60,8 @@ async (req, res) => {
 
 //find movie by title
 app.get("/movies/:title", passport.authenticate('jwt', {session: false}),
-async (req, res) => {
- await movies.findOne({title: req.params.title})
+(req, res) => {
+ movies.findOne({title: req.params.title})
   .then((movie) => {
     res.json(movie);
   }).catch((err) => {
@@ -68,8 +72,8 @@ async (req, res) => {
 
 //find a certain genre
 app.get("/movies/genre/:name", passport.authenticate('jwt', {session: false}),
-async (req, res) => {
- await movies.findOne({'genre.name' : req.params.name})
+(req, res) => {
+ movies.findOne({'genre.name' : req.params.name})
   .then((movie) => {
     res.json(movie.genre);
   }).catch((err) => {
@@ -80,8 +84,8 @@ async (req, res) => {
 
 //find director by name
 app.get("/movies/director/:name", passport.authenticate('jwt', {session: false}),
-async (req, res) => {
- await movies.findOne({'director.name': req.params.name})
+(req, res) => {
+movies.findOne({'director.name': req.params.name})
   .then((movie) => {
     res.json(movie.director);
   }).catch((err) => {
@@ -91,7 +95,17 @@ async (req, res) => {
 });
 
 //create a new user
-app.post("/users", (req, res) => {
+app.post("/users",[
+  check('name', 'Username is required').isLength({min: 5}),
+  check('name', 'Username contains non alphanumric characters - not allowed.').isAlphanumeric(),
+  chec('password', 'Password is required').not().isEmpty(),
+  check('email', 'Email does not appear to be vaild').isEmail()
+], (req, res) => {
+  let errors = validationResult(req)
+  if(!errors.isEmpty()){
+    return res.status(422).json({errors: errors.array()})
+  }
+  let hashPassword = users.hashPassword(req.body.password);
   users.findOne({name: req.body.name})
   .then((user) => {
     if (user){
@@ -110,15 +124,19 @@ app.post("/users", (req, res) => {
       })
     }
   })
+  .catch((error) => {
+    console.error(error)
+    res.status(500).send('Error ' + error)
+  })
 });
 
 //update users info 
 app.put("/users/:name", passport.authenticate('jwt', {session: false}),
- async(req, res) => {
+(req, res) => {
   if (req.user.name !== req.params.name) {
     return res.status(400).send('Permission denied')
   }
-   await users.findOneAndUpdate(
+  users.findOneAndUpdate(
     {name: req.params.name},
     {$set: {
       name: req.body.name,
@@ -134,8 +152,8 @@ app.put("/users/:name", passport.authenticate('jwt', {session: false}),
 
 //delete user by name
 app.delete("/users/:name", passport.authenticate('jwt', {session: false}),
-async (req, res) => {
- await users.findOneAndRemove({name: req.params.name})
+(req, res) => {
+users.findOneAndRemove({name: req.params.name})
   .then((user) => {
     if (!user) {
       res.status(400).send(req.params.name + ' was not found')
@@ -150,11 +168,11 @@ async (req, res) => {
 
 //add movies to users array
 app.post("/users/:name/movies/:movieID", passport.authenticate('jwt', {session: false}),
-async (req, res) => {
- await users.findOneAndUpdate({name: req.params.name}, 
+(req, res) => {
+users.findOneAndUpdate({name: req.params.name}, 
     {$push: {favoriteMovies: req.params.movieID}},
     {new: true}).then((user) => {
-      res.status(201).json(user)
+      res.status(200).json(user)
     }).catch((err) => {
       console.error(err);
       res.status(500).send('Error ' + err)
@@ -164,8 +182,8 @@ async (req, res) => {
 
 //remove movies from users array
 app.delete("/users/:name/movies/:movieID", passport.authenticate('jwt', {session: false}),
-async (req, res) => {
- await users.findOneAndUpdate({name: req.params.name}, 
+(req, res) => {
+ users.findOneAndUpdate({name: req.params.name}, 
     {$pull: {favoriteMovies: req.params.movieID}},
     {new: true}).then((user) => {
       res.status(201).json(user)
@@ -176,6 +194,6 @@ async (req, res) => {
 });
 
 
-app.listen(3000, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`http://${HOST}:${PORT}`);
 });
--- models.js
+++ models.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt');
+const passport = require('passport');
 
 let movieSchema = mongoose.Schema({
     title: {type: String, required: true},
@@ -23,6 +25,14 @@ let userSchema = mongoose.Schema({
     birthday: Date,
     favoriteMovies: [{type: mongoose.Schema.Types.ObjectId, ref: 'Movie'}]
 });
+
+userSchema.statics.hashPassword = (password) => {
+    return bcrypt.hashSync(password, 10)
+}
+
+userSchema.methods.validatePassword = function (password) {
+    return bcrypt.compareSync(password, this.password)
+}
 
 let movie = mongoose.model('movie', movieSchema);
 let user = mongoose.model('user', userSchema);
