083b9165751db625f716a368692882a8b8318b75
nnnzrnk <127133520+nnnzrnk@users.noreply.github.com>
2023-09-20 12:35:25 +0200

    one more try

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const movies = Models.movie;
 const users = Models.user;
 // mongoose.connect('mongodb://127.0.0.1:27017/cfDB', {useNewUrlParser: true, useUnifiedTopology: true});
 
-MongoClient.connect(process.env.CONNECTION_URI, {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(process.env.CONNECTION_URI, {useNewUrlParser: true, useUnifiedTopology: true});
 app.use(bodyParser.json());
 
 app.use(express.static('public'));
